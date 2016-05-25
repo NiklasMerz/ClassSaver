03a1316605527dee5aefdf93e8c69cd7f6fe02e0
Niklas <nm4500@rhoen.de>
2016-05-25 19:12:56 +0200

    Open file and write
    Signed-off-by: Niklas <nm4500@rhoen.de>

--- src/de/niklasmerz/classsaver/handlers/SampleHandler.java
+++ src/de/niklasmerz/classsaver/handlers/SampleHandler.java
@@ -1,14 +1,20 @@
 package de.niklasmerz.classsaver.handlers;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
@@ -24,11 +30,22 @@ public class SampleHandler extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		String path = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
+		path = path + "/test/src/test/test.java";
+
+		try {
+			FileWriter out = new FileWriter(path, true);
+			out.write("");
+			out.close();
+		} catch(FileNotFoundException e){
+			path = "Not found: " + path;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"ClassSaver",
-				"Hello, Eclipse world");
+		MessageDialog.openInformation(window.getShell(), "ClassSaver", path);
 		return null;
 	}
 }
